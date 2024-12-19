ab8f998010bcb3f5a2bcaf04c3c177b824b9d27f
jamessenior642 <jvsenior02@gmail.com>
2024-12-19 17:13:26 -0500

    Finished 3.7 and 3.8 - added morgan middleware for logging requests

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+// Define a custom token to log the request body
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
 
 let persons = [
     { 
